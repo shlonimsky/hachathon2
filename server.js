a991ee297355afc7407cfc432d4339d0e5a23959
shlonimsky <ashlonimsky@gmail.com>
2023-04-23 13:50:11 +0300

    removing express static

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors())
 
 app.use(express.json())
-app.use('/',express.static(__dirname+'/public'))
+// app.use('/',express.static(__dirname+'/public'))
 app.use(express.urlencoded({extended:true}))
 
 app.listen(process.env.PORT, () => console.log(`LISTENING  PORT ${process.env.PORT}`))
