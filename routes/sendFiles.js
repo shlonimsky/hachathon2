5231f4680f18a583d24d680ecebf13e42e7c50b3
shlonimsky <ashlonimsky@gmail.com>
2023-06-06 12:26:46 +0300

    path in sendFiles

--- routes/sendFiles.js
+++ routes/sendFiles.js
@@ -3,32 +3,37 @@ const path = require('path')
 
 const router = express.Router()
 
-router.get('/',(req,res) => {
-    // res.sendFile(__dirname+'/public/index.html')
-    res.sendFile(__dirname+'/public/teams.html')
-
-})
 
 router.get('/teams',(req,res) => {
-    res.sendFile(__dirname+'/public/teams.html')
+    // console.log(__dirname);
+    // res.sendFile('/teams.html')
+    // res.sendFile('/teams.html')
+    // console.log(__dirname+'/../public/teams.html');
+//   res.sendFile('/Users/anastasiiashlonimsky/Documents/Full_Stack_JS/Hackathon/hachathon2/public/teams.html')
+  res.sendFile(path.join(__dirname,'..','./public', 'teams.html'))
+
+
 })
 
 router.get('/teams/edit/:id',(req,res) => {
-    res.sendFile(__dirname+'/public/teams_edit.html')
+    res.sendFile(path.join(__dirname,'..','./public','teams_edit.html'))
 })
 
 router.get('/settings',(req,res) => {
-    res.sendFile(__dirname+'/public/settings.html')
+    res.sendFile(path.join(__dirname,'..','./public', 'settings.html'))
 })
 
 router.get('/score',(req,res) => {
-    res.sendFile(__dirname+'/public/score.html')
+    res.sendFile(_path.join(__dirname,'..','./public', 'score.html'))
 })
 
 router.get('/game',(req,res) => {
-    res.sendFile(__dirname+'/public/game.html')
+    res.sendFile(path.join(__dirname,'..','./public','game.html'))
 })
-
+// router.get('/',(req,res) => {
+//     console.log(__dirname);
+//     res.sendFile(__dirname+'/public/index.html')
+// })
 
 
 module.exports = router
