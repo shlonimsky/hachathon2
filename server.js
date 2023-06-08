d1f8d86a095b501afa2998b547d5ce5166101214
shlonimsky <ashlonimsky@gmail.com>
2023-06-08 14:17:51 +0300

    ttest2

--- server.js
+++ server.js
@@ -14,11 +14,14 @@ const app = express();
 app.use(cors())
 
 app.use(express.json())
-app.use('/',express.static(path.join(__dirname, './public')))
+// app.use('/',express.static(path.join(__dirname, './public')))
 app.use(express.urlencoded({extended:true}))
 
 app.listen(process.env.PORT, () => console.log(`LISTENING  PORT ${process.env.PORT}`))
+app.get('/', (req,res) => {
+res.send("helooooo")
 
+})
 
 app.use('/api/colors', router_colors)
 app.use('/api/words', router_words)
