0bf398c0c1f5d786a57b33d988271bff54558ab1
shlonimsky <ashlonimsky@gmail.com>
2023-06-08 14:27:11 +0300

    nastya

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 app.listen(process.env.PORT, () => console.log(`LISTENING  PORT ${process.env.PORT}`))
-app.get('/', (req,res) => {
+app.get('/nastya', (req,res) => {
 res.send("helooooo")
 
 })
